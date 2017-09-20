9eaa26ba5a644abf7f5cdd852d7f8539eff2a5ef
Tony Freeman <tonyfreeman@users.noreply.github.com>
2017-09-20 05:16:44 +0300

    Add BufferedStream

---
+++ Sources/Stream/BufferedStream.swift
@@ -0,0 +1,147 @@
+public class BufferedInputStream<T: InputStream>: InputStream {
+    public let baseStream: T
+
+    let storage: UnsafeMutableRawBufferPointer
+    var position: Int
+    var count: Int
+
+    var buffered: Int {
+        return count - position
+    }
+
+    public init(stream: T, capacity: Int = 4096) {
+        self.baseStream = stream
+        self.storage = UnsafeMutableRawBufferPointer.allocate(count: capacity)
+        self.position = 0
+        self.count = 0
+    }
+
+    @inline(__always)
+    private func flush(to buffer: UnsafeMutableRawBufferPointer) -> Int {
+        assert(buffer.count > buffered)
+        buffer.copyBytes(from: storage[position..<count])
+        let flushed = buffered
+        position = 0
+        count = 0
+        return flushed
+    }
+
+    @inline(__always)
+    private func read(count: Int) -> UnsafeMutableRawBufferPointer.SubSequence {
+        assert(buffered >= count)
+        position += count
+        return storage[position-count..<position]
+    }
+
+    public func read(to buffer: UnsafeMutableRawBufferPointer) throws -> Int {
+        switch buffered - buffer.count {
+        case 0...:
+            buffer.copyBytes(from: read(count: buffer.count))
+            return buffer.count
+        // we don't have enough data and can buffer the rest after read
+        case -(storage.count-1)..<0:
+            let flushed = flush(to: buffer)
+            count = try baseStream.read(to: storage)
+            let remain = min(count, buffer.count - flushed)
+            UnsafeMutableRawBufferPointer(rebasing: buffer[flushed...])
+                .copyBytes(from: read(count: remain))
+            return flushed + remain
+        // requested more than we can buffer, read directly to the buffer
+        default:
+            switch buffered {
+            case 0: // fast path if we always read more than capacity
+                return try baseStream.read(to: buffer)
+            default:
+                let flushed = flush(to: buffer)
+                let read = try baseStream.read(
+                    to: UnsafeMutableRawBufferPointer(
+                        rebasing: buffer[flushed...]))
+                return flushed + read
+            }
+        }
+    }
+}
+
+public class BufferedOutputStream<T: OutputStream>: OutputStream {
+    public let baseStream: T
+
+    let storage: UnsafeMutableRawBufferPointer
+    var buffered: Int
+
+    var available: Int {
+        return storage.count - buffered
+    }
+
+    public init(stream: T, capacity: Int = 4096) {
+        baseStream = stream
+        storage = UnsafeMutableRawBufferPointer.allocate(count: capacity)
+        buffered = 0
+    }
+
+    public func write(_ bytes: UnsafeRawBufferPointer) throws -> Int {
+        switch available - bytes.count {
+        case 0...:
+            UnsafeMutableRawBufferPointer(rebasing: storage[buffered...])
+                .copyBytes(from: bytes)
+            buffered += bytes.count
+            if buffered == storage.count {
+                try flush()
+            }
+            return bytes.count
+        // the buffer is full, copy as much as we can, flush, buffer the rest
+        case -(storage.count-1)..<0:
+            let count = available
+            UnsafeMutableRawBufferPointer(rebasing: storage[buffered...])
+                .copyBytes(from: bytes[..<count])
+            buffered += count
+            try flush()
+            storage.copyBytes(from: bytes[count...])
+            buffered += bytes.count - count
+            return bytes.count
+        default:
+            var sent = 0
+            if buffered > 0 {
+                try flush()
+            }
+            sent += try baseStream.write(bytes)
+            return sent
+        }
+    }
+
+    @discardableResult
+    public func flush() throws -> Int {
+        var sent = 0
+        while sent < buffered {
+            sent += try baseStream.write(
+                UnsafeRawBufferPointer(rebasing: storage[sent..<buffered]))
+        }
+        buffered = 0
+        return sent
+    }
+}
+
+public class BufferedStream<T: Stream>: Stream {
+    public let inputStream: BufferedInputStream<T>
+    public let outputStream: BufferedOutputStream<T>
+
+    @inline(__always)
+    public init(stream: T, capacity: Int = 4096) {
+        inputStream = BufferedInputStream(stream: stream, capacity: capacity)
+        outputStream = BufferedOutputStream(stream: stream, capacity: capacity)
+    }
+
+    @inline(__always)
+    public func read(to buffer: UnsafeMutableRawBufferPointer) throws -> Int {
+        return try inputStream.read(to: buffer)
+    }
+
+    @inline(__always)
+    public func write(_ bytes: UnsafeRawBufferPointer) throws -> Int {
+        return try outputStream.write(bytes)
+    }
+
+    @inline(__always)
+    public func flush() throws {
+        try outputStream.flush()
+    }
+}
---
+++ Tests/StreamTests/BufferedStreamTests.swift
@@ -0,0 +1,144 @@
+import Test
+@testable import Stream
+
+class BufferedStreamTests: TestCase {
+    class TestInputStreamSequence: InputStream {
+        func read(to buffer: UnsafeMutableRawBufferPointer) throws -> Int {
+            for i in 0..<buffer.count {
+                buffer[i] = UInt8(truncatingIfNeeded: i)
+            }
+            return buffer.count
+        }
+    }
+
+    class TestStream: Stream {
+        var storage = [UInt8]()
+
+        func read(to buffer: UnsafeMutableRawBufferPointer) throws -> Int {
+            let count = min(storage.count, buffer.count)
+            buffer.copyBytes(from: storage[..<count])
+            storage = [UInt8](storage[count...])
+            return count
+        }
+
+        func write(_ bytes: UnsafeRawBufferPointer) throws -> Int {
+            storage.append(contentsOf: bytes)
+            return bytes.count
+        }
+    }
+
+    func testBufferedInputStream() {
+        let baseStream = TestInputStreamSequence()
+        let stream = BufferedInputStream(stream: baseStream, capacity: 10)
+        assertEqual(stream.storage.count, 10)
+        assertEqual(stream.buffered, 0)
+
+        func read(count: Int) -> [UInt8] {
+            var buffer = [UInt8](repeating: 0, count: count)
+            assertEqual(try stream.read(to: &buffer), count)
+            return buffer
+        }
+
+        assertEqual(read(count: 5), [0,1,2,3,4])
+        assertEqual(stream.buffered, 5)
+        assertEqual(read(count: 2), [5,6])
+        assertEqual(stream.buffered, 3)
+        assertEqual(read(count: 13), [7,8,9,0,1,2,3,4,5,6,7,8,9])
+        assertEqual(stream.buffered, 0)
+
+        assertEqual(read(count: 10), [0,1,2,3,4,5,6,7,8,9])
+        assertEqual(stream.buffered, 0)
+
+        assertEqual(read(count: 13), [0,1,2,3,4,5,6,7,8,9,10,11,12])
+        assertEqual(stream.buffered, 0)
+
+        assertEqual(read(count: 9), [0,1,2,3,4,5,6,7,8])
+        assertEqual(stream.buffered, 1)
+        assertEqual(read(count: 13), [9,0,1,2,3,4,5,6,7,8,9,10,11])
+        assertEqual(stream.buffered, 0)
+    }
+
+    func testBufferedOutputStream() {
+        let testStream = TestStream()
+        let stream = BufferedOutputStream(stream: testStream, capacity: 10)
+        assertEqual(stream.storage.count, 10)
+        assertEqual(stream.buffered, 0)
+
+        assertEqual(try stream.write([0,1,2,3,4]), 5)
+        assertEqual(stream.buffered, 5)
+        assertEqual(try stream.write([5,6]), 2)
+        assertEqual(stream.buffered, 7)
+        assertEqual(try stream.write([7,8,9]), 3)
+        assertEqual(stream.buffered, 0)
+
+        assertEqual(testStream.storage, [0,1,2,3,4,5,6,7,8,9])
+        testStream.storage = []
+
+        assertEqual(try stream.write([0,1,2,3,4,5,6,7,8]), 9)
+        assertEqual(stream.buffered, 9)
+        assertEqual(try stream.write([9,0,1,2,3,4,5,6,7,8,9,10,11]), 13)
+        assertEqual(stream.buffered, 0)
+
+        assertEqual(testStream.storage, [
+            0,1,2,3,4,5,6,7,8,
+            9,0,1,2,3,4,5,6,7,8,9,10,11
+        ])
+        testStream.storage = []
+
+        assertEqual(try stream.write([0,1,2,3,4,5,6,7,8]), 9)
+        assertEqual(stream.buffered, 9)
+        assertEqual(try stream.write([9,0,1]), 3)
+        assertEqual(stream.buffered, 2)
+
+        assertEqual(testStream.storage, [0,1,2,3,4,5,6,7,8,9])
+        testStream.storage = []
+
+        assertEqual(try stream.flush(), 2)
+        assertEqual(stream.buffered, 0)
+
+        assertEqual(testStream.storage, [0,1])
+    }
+
+    func testBufferedStream() {
+        let stream = BufferedStream(stream: TestStream(), capacity: 10)
+
+        func read(count: Int) -> [UInt8] {
+            var buffer = [UInt8](repeating: 0, count: count)
+            guard let count = try? stream.read(to: &buffer) else {
+                fail()
+                return []
+            }
+            return [UInt8](buffer.prefix(upTo: count))
+        }
+
+        assertEqual(try stream.write([0,1,2,3,4]), 5)
+        assertEqual(stream.outputStream.buffered, 5)
+
+        assertEqual(read(count: 5), [])
+        assertEqual(stream.inputStream.buffered, 0)
+        assertNoThrow(try stream.flush())
+        assertEqual(stream.outputStream.buffered, 0)
+        assertEqual(read(count: 5), [0,1,2,3,4])
+        assertEqual(stream.inputStream.buffered, 0)
+    }
+
+    func testBufferedInputStreamDefaultCapacity() {
+        let stream = BufferedInputStream(stream: TestStream())
+        assertEqual(stream.storage.count, 4096)
+        assertEqual(stream.buffered, 0)
+    }
+
+    func testBufferedOutputStreamDefaultCapacity() {
+        let stream = BufferedOutputStream(stream: TestStream())
+        assertEqual(stream.storage.count, 4096)
+        assertEqual(stream.buffered, 0)
+    }
+
+    func testBufferedStreamDefaultCapacity() {
+        let stream = BufferedStream(stream: TestStream())
+        assertEqual(stream.inputStream.storage.count, 4096)
+        assertEqual(stream.inputStream.buffered, 0)
+        assertEqual(stream.outputStream.storage.count, 4096)
+        assertEqual(stream.outputStream.buffered, 0)
+    }
+}
