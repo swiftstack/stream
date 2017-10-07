e9d2bfbea55ec5adf4248a96430c4913d7fbe1b0
Tony Freeman <tonyfreeman@users.noreply.github.com>
2017-10-07 12:12:07 +0300

    Implement copyBytes for OutputStream

---
+++ Sources/Stream/OutputStream+CopyBytes.swift
@@ -0,0 +1,28 @@
+extension OutputStream {
+    func copyBytes<T: InputStream>(
+        from input: T,
+        bufferSize: Int = 4096
+    ) throws -> Int {
+        var total = 0
+        var buffer = [UInt8](repeating: 0, count: bufferSize)
+
+        while true {
+            let read = try input.read(to: &buffer)
+            guard read > 0 else {
+                return total
+            }
+            total = total &+ read
+
+            var index = 0
+            while index < read {
+                let written = try write(buffer[index..<read])
+                guard written > 0 else {
+                    throw StreamError.writtenLessThenRequired
+                }
+                index += written
+            }
+
+            return total
+        }
+    }
+}
