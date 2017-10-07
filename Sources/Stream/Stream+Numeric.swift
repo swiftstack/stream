756cbaeb1664315eb70928da16e9f2845d14926e
Tony Freeman <tonyfreeman@users.noreply.github.com>
2017-10-07 12:11:22 +0300

    Add Numeric extensions

---
+++ Sources/Stream/Stream+Numeric.swift
@@ -0,0 +1,24 @@
+extension InputStream {
+    @inline(__always)
+    public func read<T: BinaryInteger>(_ type: T.Type) throws -> T {
+        var result: T = 0
+        try withUnsafeMutableBytes(of: &result) { bytes in
+            guard try read(to: bytes) == MemoryLayout<T>.size else {
+                throw StreamError.readLessThenRequired
+            }
+        }
+        return result
+    }
+}
+
+extension OutputStream {
+    @inline(__always)
+    public func write<T: BinaryInteger>(_ value: T) throws {
+        var value = value
+        try withUnsafeBytes(of: &value) { bytes in
+            guard try write(bytes) == MemoryLayout<T>.size else {
+                throw StreamError.writtenLessThenRequired
+            }
+        }
+    }
+}
--- Sources/Stream/Stream.swift
+++ Sources/Stream/Stream.swift
@@ -1,3 +1,8 @@
+public enum StreamError: Error {
+    case readLessThenRequired
+    case writtenLessThenRequired
+}
+
 public protocol Stream: InputStream, OutputStream {}
 
 public protocol InputStream {
