4efd85f08fc8eb44b268fbcfe4bdbfd560a7f4e3
Tony Freeman <tonyfreeman@users.noreply.github.com>
2018-03-27 21:40:27 +0300

    [Tests] Fix warning

--- Tests/StreamTests/StreamTests.swift
+++ Tests/StreamTests/StreamTests.swift
@@ -26,7 +26,7 @@ class StreamTests: TestCase {
 
     func testCopyBytes() {
         do {
-            var input = TestStream()
+            let input = TestStream()
             let output = TestStream()
 
             assertEqual(try input.write(from: [0,1,2,3,4,5,6,7,8,9]), 10)
