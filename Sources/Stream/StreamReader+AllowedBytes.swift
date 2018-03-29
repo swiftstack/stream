07e456e862905df784ffdb39f7113d39eba8eee6
Tony Freeman <tonyfreeman@users.noreply.github.com>
2018-03-29 16:12:17 +0300

    [StreamReader] Add AllowedBytes

---
+++ Sources/Stream/StreamReader+AllowedBytes.swift
@@ -0,0 +1,68 @@
+public final class AllowedBytes {
+    @_versioned
+    let buffer: UnsafeBufferPointer<Bool>
+
+    public init(byteSet set: Set<UInt8>) {
+        let buffer = UnsafeMutableBufferPointer<Bool>.allocate(capacity: 256)
+        buffer.initialize(repeating: false)
+        for byte in set {
+            buffer[Int(byte)] = true
+        }
+        self.buffer = UnsafeBufferPointer(buffer)
+    }
+
+    public init(asciiTable table: AllowedASCII) {
+        let buffer = UnsafeMutableBufferPointer<Bool>.allocate(capacity: 256)
+        buffer.initialize(repeating: false)
+
+        var copy = table
+        let pointer = UnsafeMutableRawPointer(mutating: &copy)
+            .assumingMemoryBound(to: Bool.self)
+        let asciiBuffer = UnsafeBufferPointer(start: pointer, count: 128)
+        _ = buffer.initialize(from: asciiBuffer)
+
+        self.buffer = UnsafeBufferPointer(buffer)
+    }
+
+    deinit {
+        buffer.deallocate()
+    }
+}
+
+extension StreamReader {
+    @inline(__always)
+    public func read<T>(
+        allowedBytes: AllowedBytes,
+        body: (UnsafeRawBufferPointer) throws -> T) throws -> T
+    {
+        let buffer = allowedBytes.buffer
+        return try read(
+            while: { buffer[Int($0)] },
+            untilEnd: true,
+            body: body)
+    }
+
+    @inline(__always)
+    public func read(allowedBytes: AllowedBytes) throws -> [UInt8] {
+        let buffer = allowedBytes.buffer
+        return try read(while: { buffer[Int($0)] })
+    }
+}
+
+public typealias AllowedASCII = (
+    Bool, Bool, Bool, Bool, Bool, Bool, Bool, Bool,
+    Bool, Bool, Bool, Bool, Bool, Bool, Bool, Bool,
+    Bool, Bool, Bool, Bool, Bool, Bool, Bool, Bool,
+    Bool, Bool, Bool, Bool, Bool, Bool, Bool, Bool,
+    Bool, Bool, Bool, Bool, Bool, Bool, Bool, Bool,
+    Bool, Bool, Bool, Bool, Bool, Bool, Bool, Bool,
+    Bool, Bool, Bool, Bool, Bool, Bool, Bool, Bool,
+    Bool, Bool, Bool, Bool, Bool, Bool, Bool, Bool,
+    Bool, Bool, Bool, Bool, Bool, Bool, Bool, Bool,
+    Bool, Bool, Bool, Bool, Bool, Bool, Bool, Bool,
+    Bool, Bool, Bool, Bool, Bool, Bool, Bool, Bool,
+    Bool, Bool, Bool, Bool, Bool, Bool, Bool, Bool,
+    Bool, Bool, Bool, Bool, Bool, Bool, Bool, Bool,
+    Bool, Bool, Bool, Bool, Bool, Bool, Bool, Bool,
+    Bool, Bool, Bool, Bool, Bool, Bool, Bool, Bool,
+    Bool, Bool, Bool, Bool, Bool, Bool, Bool, Bool)
