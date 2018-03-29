82c212ab24700e520554ace7f1748447796bde8b
Tony Freeman <tonyfreeman@users.noreply.github.com>
2018-03-29 17:46:51 +0300

    Add UnsafeRawBufferInitializable

---
+++ Sources/Stream/UnsafeRawBufferInitializable.swift
@@ -0,0 +1,53 @@
+public protocol UnsafeRawBufferInitializable {
+    init(_ buffer: UnsafeRawBufferPointer)
+}
+
+extension String: UnsafeRawBufferInitializable {
+    public init(_ buffer: UnsafeRawBufferPointer) {
+        self.init(decoding: buffer, as: UTF8.self)
+    }
+}
+
+extension Array: UnsafeRawBufferInitializable where Element == UInt8 {}
+
+extension StreamReader {
+    @_inlineable
+    public func peek<T>(count: Int, as type: T.Type) throws -> T
+        where T: UnsafeRawBufferInitializable
+    {
+        return try peek(count: count) { bytes in
+            return T(bytes)
+        }
+    }
+
+    @_inlineable
+    public func read<T>(count: Int, as type: T.Type) throws -> T
+        where T: UnsafeRawBufferInitializable
+    {
+        return try read(count: count) { bytes in
+            return T(bytes)
+        }
+    }
+
+    @_inlineable
+    public func read<T: UnsafeRawBufferInitializable>(
+        mode: PredicateMode,
+        while predicate: (UInt8) -> Bool,
+        as type: T.Type) throws -> T
+    {
+        return try read(mode: mode, while: predicate) { bytes in
+            return T(bytes)
+        }
+    }
+
+    @_inlineable
+    public func readUntilEnd<T>(as type: T.Type) throws -> T
+        where T: UnsafeRawBufferInitializable
+    {
+        return try read(mode: .untilEnd, while: {_ in true}, as: type)
+    }
+
+    public func readUntilEnd() throws -> [UInt8] {
+        return try readUntilEnd(as: [UInt8].self)
+    }
+}
