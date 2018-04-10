06ca58fcfb9250d5a545d109a4024cf15016d85e
Tony Freeman <tonyfreeman@users.noreply.github.com>
2018-04-10 22:30:48 +0300

    [NFC] Remove debug print

--- Sources/Stream/SubStreamWriter.swift
+++ Sources/Stream/SubStreamWriter.swift
@@ -15,7 +15,6 @@ extension StreamWriter {
     {
         let output = OutputByteStream()
         try task(output)
-        print(output.bytes.count)
         try write(T(output.bytes.count))
         try write(output.bytes)
     }
