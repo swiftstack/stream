377126cc7ed83434bc590eba8ecadc4f44246f19
Tony Freeman <tonyfreeman@users.noreply.github.com>
2021-03-07 02:53:25 +0300

    [Tests] Fix SubStreamReader tests

--- Tests/Stream/SubStreamReader/main.swift
+++ Tests/Stream/SubStreamReader/main.swift
@@ -2,11 +2,11 @@ import Test
 @testable import Stream
 
 test.case("LimitedBy") {
-    let stream = ByteArrayInputStream([UInt8]("Hello, World!".utf8))
+    let stream = ByteArrayInputStream("Hello, World!") as StreamReader
     let hello = try await stream.withSubStreamReader(limitedBy: 5) { stream in
         return try await stream.readUntilEnd(as: String.self)
     }
-    try stream.consume(count: 2)
+    try await stream.consume(count: 2)
     let world = try await stream.readUntilEnd(as: String.self)
     expect(hello == "Hello")
     expect(world == "World!")
@@ -14,12 +14,12 @@ test.case("LimitedBy") {
 
 test.case("SizedBy") {
     let bytes = [0x00, 0x05] + [UInt8]("Hello, World!".utf8)
-    let stream = ByteArrayInputStream(bytes)
+    let stream = ByteArrayInputStream(bytes) as StreamReader
     let hello = try await stream.withSubStreamReader(sizedBy: UInt16.self)
     { stream in
         return try await stream.readUntilEnd(as: String.self)
     }
-    try stream.consume(count: 2)
+    try await stream.consume(count: 2)
     let world = try await stream.readUntilEnd(as: String.self)
     expect(hello == "Hello")
     expect(world == "World!")
@@ -27,14 +27,14 @@ test.case("SizedBy") {
 
 test.case("SizedByIncludingHeader") {
     let bytes = [0x00, 0x07] + [UInt8]("Hello, World!".utf8)
-    let stream = ByteArrayInputStream(bytes)
+    let stream = ByteArrayInputStream(bytes) as StreamReader
     let hello = try await  stream.withSubStreamReader(
         sizedBy: UInt16.self,
         includingHeader: true)
     { stream in
         return try await stream.readUntilEnd(as: String.self)
     }
-    try stream.consume(count: 2)
+    try await stream.consume(count: 2)
     let world = try await stream.readUntilEnd(as: String.self)
     expect(hello == "Hello")
     expect(world == "World!")
